8708996a600afb2f57ac7db6343c9d90062c5360
Chad Quilling <chadquilling@gmail.com>
2021-10-15 16:56:28 -0600

    Output information about the holidays.

--- scripts/script.js
+++ scripts/script.js
@@ -4,17 +4,29 @@
 // The Image api
 // https://www.pexels.com/api/documentation/#authorization
 
-function httpGetAsync(url, callback) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
-        callback(xmlHttp.responseText);
-    }
-    xmlHttp.open("GET", url, true); // true for asynchronous
-    xmlHttp.send(null);
-}
+const month_list = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-var url = "https://holidays.abstractapi.com/v1/?api_key=effdd4458cac464698cb3c7c9eb78893&country=US&year=2020&month=12&day=25"
+function updateResults(holidayJson) {
+  let results = "";
+  const day = document.getElementById("day").value;
+  const month = document.getElementById("month").value;
+  const year = document.getElementById("year").value;
+
+  if (holidayJson.length === 0) { // There is no holidays
+    results += `<h2>There is no holiday on ${day}/${month}/${year}</h2>`;
+    document.getElementById("results").innerHTML = results;
+    return;
+  }
+
+  results += `<h2>Holidays on ${holidayJson[0].week_day}, ${day} ${month_list[month]} ${year} in the US:</h2>`;
+  for (let i = 0; i < holidayJson.length; i++) {
+    results += "<div class='holiday'>";
+    results += `<h3>${holidayJson[i].name}</h3>`;
+    results += `<h4>Recognized because of: ${holidayJson[i].name}</h4>`;
+    results += "</div>";
+  }
+  document.getElementById("results").innerHTML = results;
+}
 
 // httpGetAsync(url)
 
@@ -25,7 +37,7 @@ document.getElementById("submit").addEventListener("click", function(event) {
   const month = document.getElementById("month").value;
   const year = document.getElementById("year").value;
 
-  if (day === "" ||  month === "" || year === "")
+  if (day === "" || month === "" || year === "")
     return;
 
   const months_w_30_days = ['4', '6', '5', '9', '11'];
@@ -49,5 +61,5 @@ document.getElementById("submit").addEventListener("click", function(event) {
       console.log(json);
     });
 
-    // document.getElementById("results").style.display = "flex";
+  // document.getElementById("results").style.display = "flex";
 });
