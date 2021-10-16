f6690365e6566252504a99079cb00e313854e235
Chad Quilling <chadquilling@gmail.com>
2021-10-15 20:40:55 -0600

    remove pexels and fix import issues.

--- scripts/pexels/src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const photoBaseUrl = "https://api.pexels.com/v1/";
-export const videoBaseUrl = "https://api.pexels.com/videos/";
-export const collectionBaseUrl = "https://api.pexels.com/v1/collections/";
-export const testApiKey = process.env.API_KEY as string;
